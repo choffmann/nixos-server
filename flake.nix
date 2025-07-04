2a3f3869adecc886f104cbabf315fd85e16dd8a8
Cedrik Hoffmann <dev@choffmann.io>
2025-07-04 13:34:27 +0200

    feat: setup sops secrets

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
+  inputs.sops-nix.url = "github:Mic92/sops-nix";
+  inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
   outputs = {
     nixpkgs,
     disko,
+    sops-nix,
     ...
   }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
@@ -20,6 +24,7 @@
     nixosConfigurations.matrix-server = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        sops-nix.nixosModules.sops
         disko.nixosModules.disko
         ./machines/matrix-server/configuration.nix
       ];
@@ -27,7 +32,11 @@
 
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
-        packages = with pkgs; [nixos-anywhere];
+        packages = with pkgs; [
+          nixos-anywhere
+          sops
+          ssh-to-age
+        ];
       };
     });
   };
--- machines/matrix-server/configuration.nix
+++ machines/matrix-server/configuration.nix
@@ -19,20 +19,35 @@
   };
   services.openssh.enable = true;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  sops.defaultSopsFile = ../../secrets/matrix-server.yaml;
+  sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  sops.secrets.foo = {};
+  sops.secrets."homeserver.yaml" = {
+    owner = "matrix-synapse";
+    restartUnits = ["matrix-synapse.service"];
+  };
+
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
   ];
 
-  networking.hostName = "matrix-server";
   time.timeZone = "Europe/Berlin";
+  networking.hostName = "matrix";
+  networking.domain = "homebin.dev";
 
+  users.users.root.password = "1234";
+  users.users.root.hashedPasswordFile = null;
   users.users.root.openssh.authorizedKeys.keys =
     [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJw08ayERLCtV3iZtTRnGWLNJzPyGdfUUm3LUYbDPXT6 choffmann"
     ]
     ++ (args.extraPublicKeys or []); # this is used for unit-testing this module and can be removed if not needed
 
+  services.matrix-synapse.extraConfigFiles = [
+    "/run/secrets/homeserver.yaml"
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   system.stateVersion = "25.11";
 }
